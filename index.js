f4f3059dc67e956d9d88a6ae3804d4f4e5682de7
C9hop <cloud99gg@gmail.com>
2020-09-15 03:25:27 +0700

    update

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Bài 1: ");
+console.log("1. Trong JS, xảy ra lỗi syntax khi: ");
+console.log("- Biến được khởi tạo bắt đầu bằng số")
+console.log("- Biến chứa các kí tự đặc biệt(ngoại trừ _ và $)")
+console.log("- Biến được khởi tạp trùng tên với các từ khóa")
+console.log("Ví dụ")
+console.log("let 9c")
+console.log("let c9-hop")
+console.log("let null")
+console.log("2. Trong JS, để kiểm tra một kiểu dữ liệu biến:")
+console.log("Dùng lệnh typeof để xem kiểu dữ liệu của biến")
+let age = '18';
+console.log('VD: let age = "18": ' + typeof(age));
+console.log("bài 2: ");
+console.log("1-b");
+console.log("2-a");
+console.log("3-e");
+console.log("4-c");
+
+
+
+
+
+
+
