a5ce5849d3b33f54207b74b1f1815068c97d6336
C9hop <cloud99gg@gmail.com>
2020-09-15 14:19:43 +0700

    update

--- index.js
+++ index.js
@@ -11,11 +11,17 @@ console.log("2. Trong JS, để kiểm tra một kiểu dữ liệu biến:")
 console.log("Dùng lệnh typeof để xem kiểu dữ liệu của biến")
 let age = '18';
 console.log('VD: let age = "18": ' + typeof(age));
+
+
+
 console.log("Bài 2: ");
 console.log("1-b");
 console.log("2-a");
 console.log("3-e");
 console.log("4-c");
+
+
+
 console.log("Bài 3: ");
 console.log("a)");
 let message = ('Coding is great');
@@ -23,6 +29,55 @@ console.log(message);
 console.log("b)");
 let studentCount = 0;
 console.log(studentCount);
+
+
+
+console.log("Bài 4: ");
+console.log("a)");
+message = 'Coding might not be easy, but still great';
+console.log(message);
+console.log("b)");
+studentCount = 16;
+console.log(studentCount);
+console.log("c)");
+message = message.toLowerCase();
+console.log(message);
+console.log("d)");
+studentCount++;
+console.log(studentCount);
+
+
+
+console.log("Bài 5: ");
+alert('You look beautiful today');
+
+
+
+console.log("Bài 6: ");
+let name = prompt('Hi there, your name please?');
+alert('Hi ' + name);
+
+console.log("Bài 7: ");
+let FirstName = prompt('Enter your first name');
+let LastName = prompt('Enter your last name');
+alert(`Hi ${LastName} ${FirstName}`);
+
+
+
+
+console.log("Bài 8: ");
+let l = prompt('Enter side length of the square');
+let s;
+s = l*l;
+alert(`The square area is ${s}`);
+
+
+
+console.log("Bài 9: ");
+
+
+
+
 
 
 
