b5d96f0bb2694caa9234cf39936ebc60d369b741
C9hop <cloud99gg@gmail.com>
2020-09-18 11:51:18 +0700

    update

--- Session-2/hwork-6/index.js
+++
@@ -1,7 +0,0 @@
-let x = prompt('x =');
-if(x%2!=0){
-    alert(x + ' is an odd  number');
-}
-else if(x%2==0){
-    alert(x + ' is an even number');
-}
---
+++ Session-2/hwork-8/index.js
@@ -0,0 +1,7 @@
+let x = prompt('x =');
+if(x%2!=0){
+    alert(x + ' is an odd  number');
+}
+else if(x%2==0){
+    alert(x + ' is an even number');
+}
