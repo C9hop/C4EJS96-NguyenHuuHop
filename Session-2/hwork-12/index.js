e828d14ea2db2fe44db52bac84ca122d80bac851
C9hop <cloud99gg@gmail.com>
2020-09-18 14:34:37 +0700

    update

---
+++ Session-2/hwork-12/index.js
@@ -0,0 +1,29 @@
+let x = prompt('Enter number of edges')
+if (x=3){
+  function triangle() {
+    for (var i=1; i <= 3; i++) {
+      fd(50)
+      rt(120)
+    }
+  }
+  triangle()
+}
+if (x=8){
+  function octagonal() {
+    for (var i=1; i <= 8; i++) {
+      fd(50)
+      rt(45)
+    }
+  }
+  octagonal()
+}
+if (x=999){
+    function circle() {
+      for (var i=1; i <= 999; i++) {
+      
+
+        
+      }
+    }
+    circlel()
+  }
