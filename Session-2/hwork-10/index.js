f8f8835158893455a9e961d672ce491085a85c24
C9hop <cloud99gg@gmail.com>
2020-09-18 14:11:34 +0700

    update

--- Session-2/hwork-10/index.js
+++ Session-2/hwork-10/index.js
@@ -2,3 +2,19 @@ let mass = prompt('Your weight in kg?');
 let height = prompt('Your height in cm?');
 let BMI = mass/((height/100)*(height/100));
 alert(`Your BMI is ${BMI}`);
+if(BMI<16){
+    alert('You are severely underweight')
+}
+else if(16<=BMI<18,5){
+    alert('You are underweight')
+}
+else if(18,5<=BMI<25){
+    alert('You are normal')
+}
+else if(25<=BMI<30){
+    alert('You are overweight')
+}
+else {
+    alert('You are obese')
+
+}
---
+++ Session-2/hwork-9/index.js
@@ -0,0 +1,22 @@
+console.log('a)');
+for(let i=0 ; i<3; i++){
+    console.log('H')
+}
+for(let i=0 ; i<3; i++){
+    console.log('L')
+}
+
+console.log('b)');
+let x = prompt("Enter the total number of L's and H's?");
+for(let i=1 ; i<x/2; i++){
+    console.log('H')
+}
+for(let i=1 ; i<x/2; i++){
+    console.log('L')
+}
+
+console.log('c)');
+for(let i=0; i<4; i++ ){
+    console.log('0')
+    console.log('1')
+}
