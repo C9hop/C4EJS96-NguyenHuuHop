dee9c8919f978622ddebebdec22d7a255aa95b2e
C9hop <cloud99gg@gmail.com>
2020-09-18 13:20:55 +0700

    update

---
+++ Session-2/hwork-6/index.js
@@ -0,0 +1,7 @@
+let x = prompt('Enter a  number');
+if(x<9/2){
+    alert('lower half of 9');
+}
+else{
+    alert('Higher half of 9');
+}
---
+++ Session-2/hwork-7/index.js
@@ -0,0 +1,8 @@
+let n = prompt('n = ');
+let x = prompt('x = ');
+if(x<n/2){
+    alert('lower half of ' + n);
+}
+else{
+    alert('Higher half of ' + n);
+}
