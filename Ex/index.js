cad98d874e796a0511e2f77f79928992660c7bbd
C9hop <cloud99gg@gmail.com>
2020-09-22 00:19:06 +0700

    update

---
+++ Ex/index.js
@@ -0,0 +1,23 @@
+let menu = ['banh mi', 'com rang', 'coca cola', 'coffee'];
+console.log(menu);
+
+// menu.push('fami');
+// console.log(menu);
+
+// console.log(menu[2]);
+// menu[2] = 'tra thai';
+// console.log(menu[2]);
+// console.log(menu);
+
+// menu.splice(0,2);
+// console.log(menu);
+
+// for(let i = 0; i < menu.length; i++){
+//     console.log(menu[i]);
+// }
+
+for(let i = 0; i < menu.length; i++){
+    menu[i]=menu[i].toUpperCase();
+}
+console.log(menu);   
+
