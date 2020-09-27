bde6a8d2d7e8247b96d4114d030745bcc9074eca
C9hop <cloud99gg@gmail.com>
2020-09-28 02:17:26 +0700

    update

---
+++ Session-5/videos/Basic-part3/index.js
@@ -0,0 +1,78 @@
+//3.1:
+// let list = ['to', 'be', 'that', 'of', 'elon', 'to', 'this', 'now', 'back', 'cool', 'hey', 'love', 'of', 'life', 'that', 'rain', 'summer', 'color', 'now', 'of', 'hat', 'late', 'sorry', 'my', 'team']
+// console.log(list.sort());
+//3.2:
+let inventory = [
+
+    {
+    
+        name: 'HP Envy 13aq',
+    
+        price: 21000,
+    
+        brand: 'HP',
+    
+        quantity: 5,
+    
+    },
+    
+    {
+    
+        name: 'Dell XPS 9370',
+    
+        price: 30000,
+    
+        brand: 'Dell',
+    
+        quantity: 1,
+    
+    },
+    
+    {
+    
+        name: 'Dell Inspiron 3567',
+    
+        price: 9300,
+    
+        brand: 'Dell',
+    
+        quantity: 12,
+    
+    },
+    
+    {
+    
+        name: 'Dell Latitude E5450',
+    
+        price: 8600,
+    
+        brand: 'Dell',
+    
+        quantity: 2,
+    
+    },
+    
+    {
+    
+        name: 'Asus Zenbook',
+    
+        brand: 'Asus',
+    
+        price: 20000,
+    
+        quantity: 4,
+    
+    },
+    
+    {
+    
+        name: 'HP Pavilion',
+    
+        brand: 'HP',
+    
+        price: 14000,
+    
+        quantity: 7,
+    
+    },
+]
