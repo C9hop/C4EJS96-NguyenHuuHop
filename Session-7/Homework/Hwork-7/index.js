8545020445fb260a434518429e9003a2e9f9b8f9
C9hop <cloud99gg@gmail.com>
2020-10-06 22:34:10 +0700

    update

--- Session-7/Homework/Hwork-7/index.js
+++ Session-7/Homework/Hwork-7/index.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLeaded", function(){
-    const btn = document.getElementsByClassName("button1")
-    btn.addEventListener(`click` ,(e) =>{
-        console.log(e.target.btn.tagName)
+    let btn1 = document.getElementsByClassName("button1")
+    btn1.addEventListener(`click` ,(e) =>{
+        console.log(e.target.btn1);
+    })
+    let btn2 = document.getElementsByClassName("button2")
+    btn2.addEventListener(`click` ,(e) =>{
+        console.log(e.target.btn2);
     })
 })
