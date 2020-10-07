973e83cd03c4af5e5cad14d8cd53cc2ade6b92d9
C9hop <cloud99gg@gmail.com>
2020-10-07 19:18:15 +0700

    update

--- Session-7/Videos/Timer/index.js
+++ Session-7/Videos/Timer/index.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let numberEl = document.getElementById('number_input');
-    let startEl = document.getElementById('start_btn');
-    setInterval (startEl.addEventListener(`click`,()=>{
-    let time = numberEl.value;
-        time--;
-        if(time>=0){
-    let countdownEl = document.getElementById('countdown');
-            countdownEl.innerHTML = time;
-        }
-}),3000);
-})
+    document.addEventListener("DOMContentLoaded", function(){
+        let numberEl = document.getElementById('number_input');
+        let startEl = document.getElementById('start_btn');
+         startEl.addEventListener(`click`,()=>{
+           let time = numberEl.value;
+            setInterval (()=>{
+              time--;
+              if(time>=0){
+                   let countdownEl = document.getElementById('countdown');
+                   countdownEl.innerHTML = time;   
+            }},1000)})
+            })
