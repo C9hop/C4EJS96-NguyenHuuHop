30893dee45bffc361bba521cea83386ede5f755b
C9hop <cloud99gg@gmail.com>
2020-10-13 15:11:55 +0700

    update

---
+++ project-BodyOdour/Media/JS/data.js
@@ -0,0 +1,121 @@
+let admin ={
+    email:`amin123@gmail.com`,
+    password:`amin123`,
+}
+let user = [
+    {
+        name: `Nguyen Huu Hop`,
+        age: 20,
+        address: `Ha Noi`,
+        phoneNumber: 033123456,
+        email: `hop123@gmail.com`,
+        password: `hop123`,
+    },
+    {
+        name: `Phung Duc Trung`,
+        age: 20,
+        address: `Ha Noi`,
+        phoneNumber: 135791113,
+        email: `trung123@gmail.com`,
+        password: `trung123`,
+    },    {
+        name: `Do Minh Khue`,
+        age: 20,
+        address: `Ha Noi`,
+        phoneNumber: 246810121,
+        email: `khue123@gmail.com`,
+        password: `khue123`,
+    }
+]
+let productsData = [
+    {
+        name:`La Nuit De L’homme EDT`,
+        brand:`Yves Saint Laurent`,
+        price:`2.950.000đ`,
+        imageUrl:``,
+        size:`100ml`,
+        scent:[
+            'lavender',
+            'snowwood',
+            'cardamom',
+        ],
+    },{
+        name:`Libre`,
+        brand:`Yves Saint Laurent`,
+        price:`3.400.000đ`,
+        imageUrl:``,
+        size:`90ml`,
+        scent:[
+            'orange madarin',
+            'lavender',
+            'musk',
+        ],
+    }, {
+        name:`Dior Sauvage EDT`,
+        brand:`Dior`,
+        price:`3.150.000đ`,
+        imageUrl:``,
+        size:`100ml`,
+        scent:[
+            'black pepper',
+            'snowwood',
+            'ambroxan',
+        ],
+    }, {
+        name:`Sauvage Parfum`,
+        brand:`Dior`,
+        price:`3.850.000đ`,
+        imageUrl:``,
+        size:`100ml`,
+        scent:[
+            'vanilla',
+            'frankincense',
+            'sandalwood',
+
+        ],
+    }, {
+        name:`Chanel Coco Noir`,
+        brand:`Chanel`,
+        price:`4.250.000đ`,
+        imageUrl:``,
+        size:`100ml`,
+        scent:[
+            'orange',
+            'rose',
+            'tonka beans',
+        ],
+    }, {
+        name:`Chanel Chance EDT`,
+        brand:`Chanel`,
+        price:`3.400.000đ`,
+        imageUrl:``,
+        size:`100ml`,
+        scent:[
+            'pineapple',
+            'jasmine',
+            'vanilla',
+        ],
+    }, {
+        name:`Ombre Leather EDP`,
+        brand:`Tom Ford`,
+        price:`4.700.000đ`,
+        imageUrl:``,
+        size:`100ml`,
+        scent:[
+            '',
+            '',
+            '',
+        ],
+    }, {
+        name:``,
+        brand:`Tom Ford`,
+        price:`đ`,
+        imageUrl:``,
+        size:`100ml`,
+        scent:[
+            '',
+            '',
+            '',
+        ],
+    },
+]
