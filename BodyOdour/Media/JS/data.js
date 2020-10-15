640c012132b3a71e64c937281524bdd1b419a846
C9hop <cloud99gg@gmail.com>
2020-10-15 14:22:41 +0700

    update

--- BodyOdour/Media/JS/data.js
+++ BodyOdour/Media/JS/data.js
@@ -176,3 +176,47 @@ let productsData = [
         ],
     },
 ]
+let mainProd = document.getElementById('main-content');
+
+
+//fnc list
+function showList(productsData){
+    // let entry = data.entry;
+    // console.log(entry);
+    // console.log(productsData)
+    for(let prod of productsData){
+        let imageUrl = prod.imageUrl;
+        let name = prod.name;
+        // console.log(name)
+        let brand = prod.brand;
+        let price = prod.price;
+        let size = prod.size;
+        let sex = prod.sex;
+        let scent = prod.scent;
+        // let more = prod['link'][0].attributes.href;
+        product(imageUrl,name, brand,price,size,sex,scent);
+    }
+}
+// console.log(showList);
+showList(productsData);
+
+
+// fnc prod
+function product(imageUrl,name, brand,price,size,sex,scent){
+
+    // console.log(mainProd)
+    mainProd.insertAdjacentHTML('beforeend',`
+    <div class="main-item">
+        <div class="main-pic">
+            <img src="${imageUrl}" alt="${name}"/>
+        </div>
+        <div class="title">
+            <b>${brand}</b><br/>
+            <span>${price}</span>
+            <span>${size}</span>
+            <span>${sex}</span>
+            <span>${scent}</span>
+        </div>
+    </div>
+`);
+}
