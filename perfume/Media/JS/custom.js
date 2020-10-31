f65b70616e54d24fad49072862eaa8c25cd6a951
C9hop <cloud99gg@gmail.com>
2020-10-31 20:11:34 +0700

    update

---
+++ perfume/Media/JS/custom.js
@@ -0,0 +1,313 @@
+//log-in
+let modalSignUpBtn = document.getElementById(`modal_sign_up_btn`);
+let modalSignInBtn = document.getElementById(`modal_sign_in_btn`);
+let signOutBtn = document.getElementById(`sign_out_btn`);
+let modalPasswordInput = document.getElementById(`sign_in_password_input`);
+let modalNameInput = document.getElementById(`sign_in_name_input`);
+let signUpBtn = document.getElementById(`sign_up_btn`);
+let signInBtn = document.getElementById(`sign_in_btn`);
+let modalSignUpStatusTitle = document.getElementById(`sign_up_status_title`);
+let modalSignInStatusTitle = document.getElementById(`sign_in_status_title`);
+let passwordInput = document.getElementById(`sign_up_password_input`);
+let repeatPasswordInput = document.getElementById(`repeat_password_input`);
+let nameInput = document.getElementById(`sign_up_name_input`);
+let EmailInput = document.getElementById(`Email_name_input`);
+let modalSignUp = document.getElementById('id01');
+let modalSignIn = document.getElementById(`id02`);
+let sideName = document.getElementById(`side_user_name`);
+let bagRedDot = document.getElementById(`bag_red_dot`);
+let Content_Title_Bag = document.getElementById(`content_title_bag`);
+bagRedDot.style.display = `none`;
+Content_Title_Bag.style.display = `none`;
+
+// Nút đăng nhập
+function signInOnClickCallback() {
+    modalSignInStatusTitle.textContent = `Đăng nhập thành viên trang web`;
+    modalSignInStatusTitle.style.color = `black`;
+}
+signInBtn.addEventListener(`click`, signInOnClickCallback);
+// Nút đăng nhập trong phần Đăng nhập
+function modalSignInOnclickCallback() {
+  // console.log(user)
+  check = false;
+  for (let i = 0; i < user.length; i++) {
+      console.log(user[i])
+      if (user[i].email==modalNameInput.value && user[i].password==modalPasswordInput.value)
+      { signIn(i);
+            Content_Title_Bag.style.display = `block`;
+              modalNameInput.value = null;
+              modalPasswordInput.value = null;
+              modalSignIn.style.display = `none`;     
+
+          check=true
+            }
+     
+    }
+      if(check==false){
+              modalSignInStatusTitle.textContent = `Thông tin sai! Vui lòng điền lại`;
+              modalSignInStatusTitle.style.color = `red`;
+              modalNameInput.value = null;
+              modalPasswordInput.value = null;   
+      }
+}
+modalSignInBtn.addEventListener(`click`, modalSignInOnclickCallback);
+
+function signIn(index) {
+    signInBtn.style.display = `none`;
+    signOutBtn.style.display = `block`;
+    sideName.textContent = user[index].name;
+    sideName.style.display = `block`;     
+
+}
+// Nút đăng xuất
+function signOut() {
+    signInBtn.style.display = `block`;
+    signOutBtn.style.display = `none`;
+    sideName.textContent = ``;
+  Content_Title_Bag.style.display = `none`
+}
+signOutBtn.addEventListener(`click`, signOut);
+
+
+//showPro
+let mainProd = document.getElementById('main-content');
+
+function update_table() {
+
+   mainProd.innerHTML = ''; 
+  // table_body.innerHTML = '';
+
+  for (let data of productsData) {
+    mainProd.insertAdjacentHTML('beforeend', `<tr><div class="main-item"> <div class="main-pic">
+          <img class="img_pro" wirdth="170" height="170" src="${data.imageUrl}" alt="${data.name}"/>
+      </div>
+      <div class="title">
+          <b>${data.brand}</b><br/>
+          <span>${data.price} vnđ</span>
+          
+      </div></td><button class="add_btns">Buy</button><button class="view_btns" onclick="document.getElementById('view').style.display='block'">View</button></td></div></tr>`);
+    };
+}
+update_table();
+
+// //DOM add-delete
+
+const table_body = document.getElementById('body');
+const add_btns = document.getElementsByClassName('add_btns');
+const remove_btns = document.getElementsByClassName('remove_btn');
+const tableChoice = document.getElementById('table_choice');
+const number_product = document.getElementsByClassName('number_ipt');
+const ContainerCartItems = document.getElementById('container_cart_items');
+const product_choice=[ ];
+
+// function update_table() {
+
+//     // table_body.innerHTML = '';
+
+//     for (let data of productsData) {
+//         table_body.insertAdjacentHTML('beforeend', `<tr><td>${data.name}</td><td>${data.brand}</td><td>${data.price}</td><td><td>${data.size}</td><td>${data.sex}</td><td>${data.scent}</td></td><button class="add_btns">Add</button></td></tr>`);
+//       };
+// }
+// update_table();
+
+// button Add table choice
+  for (let i = 0; i < add_btns.length; i++) {
+        add_btns[i].addEventListener('click', () => {
+           let choice ={name:productsData[i].name, brand:productsData[i].brand, price:productsData[i].price, size:productsData[i].size,sex:productsData[i].sex,scent:productsData[i].scent}
+           const isAdded = product_choice.find(product => product.name === choice.name)
+           const maxlength=product_choice.length
+           if (isAdded){
+            alert('bạn đã thêm sản phẩm này rồi');
+           }
+           else if(maxlength==5)
+           {
+            alert('Xin lỗi giỏ hàng đã đầy'); 
+           }
+           else{
+           ShowModal();
+           product_choice.push(choice);
+           update_tableChoice()
+           }
+        })
+  }
+function  update_tableChoice(){
+
+  ContainerCartItems.innerHTML = '';
+
+    for (let prod of product_choice) {
+      ContainerCartItems.insertAdjacentHTML('beforeend',`<tr><td>${prod.name}</td><td>${prod.brand}</td><td>${prod.price}</td><td><input style="width=2px" type="number" min="1" placeholder="number" id="box"  class="number_ipt"></td><td><button onclick="Remove()" class="remove_btn">Delete</button></td></tr>`)
+       
+      }
+} 
+
+ // button remove table choice
+function Remove(){
+ for (let i = 0; i < remove_btns.length; i++) {
+        remove_btns[i].addEventListener('click', () => {
+            product_choice.splice(i, 1);
+          console.log(product_choice)
+           update_tableChoice();
+        });
+ }
+}
+
+function Sum() {
+  let sum=0;
+   for(let t=0 ;t<number_product.length;t++)
+    {
+    sum+=product_choice[t].price*number_product[t].value;
+    }
+    console.log(sum)
+     document.getElementById('sum').innerHTML=sum+" vnd";
+  }
+
+  //view
+const view = document.getElementById('view_perfume');
+const click_view = document.getElementsByClassName('view_btns');
+
+for (let i=0; i<click_view.length; i++) {
+  click_view[i].addEventListener('click',()=>{
+    // function view_perfume() {
+
+  view.innerHTML = ''; 
+
+  //  for (let i=0; i<productsData.length; i++) {
+     view.insertAdjacentHTML('beforeend', `<div class="info_text"> <div class="info_img">
+           <img class="img_pro" wirdth="170" height="170" src="${productsData[i].imageUrl}" alt="${productsData[i].name}"/>
+       </div>
+       <div class="title">
+           <b style="color: red;">${productsData[i].name}</b>
+           <b> Brand:</b> <div>${productsData[i].brand}</div>
+           <b> Size:</b> <div>${productsData[i].size}</div>
+           <b> Sex:</b> <div>${productsData[i].sex}</div>
+           <b> Scent:</b> <div>${productsData[i].scent}</div>
+           <span> Price: ${productsData[i].price} vnđ</span>
+           
+       </div><button class="close_btns" onclick="document.getElementById('view').style.display='none'">Close</button>`);
+    //  };
+  // }
+  // view_perfume();
+  })
+}
+
+// //Slideshow
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000);
+}
+
+//male choice
+const MaleChoice = document.getElementById('male_choice');
+var maleproduct =  productsData.filter(function(male) {
+    return male.sex == `male`;
+});
+
+console.log(maleproduct)
+
+function update_maleproduct() {
+
+  mainProd.innerHTML = '';
+
+    for (let promale of maleproduct) {
+      mainProd.insertAdjacentHTML('beforeend', `<tr><div class="main-item"> <div class="main-pic">
+      <img class="img_pro" wirdth="170" height="170" src="${promale.imageUrl}" alt="${ promale.name}"/>
+  </div>
+  <div class="title">
+      <b>${ promale.brand}</b><br/>
+      <span>${ promale.price}</span>
+      
+  </div></td><button class="add_btns">Buy</button></td></div></tr>`);
+      };
+}
+
+//female choice
+
+const FemaleChoice = document.getElementById('male_choice');
+var femaleproduct =  productsData.filter(function(female) {
+    return female.sex == `female`;
+});
+
+// console.log(femaleproduct)
+
+function update_femaleproduct() {
+
+  mainProd.innerHTML = '';
+
+    for (let profemale of femaleproduct) {
+      mainProd.insertAdjacentHTML('beforeend', `<tr><div class="main-item"> <div class="main-pic">
+      <img class="img_pro" wirdth="170" height="170" src="${profemale.imageUrl}" alt="${ profemale.name}"/>
+  </div>
+  <div class="title">
+      <b>${ profemale.brand}</b><br/>
+      <span>${ profemale.price}</span>
+      
+  </div></td><button class="add_btns">Buy</button></td></div></tr>`);
+      };
+}
+
+//unisex choice
+
+var unisexproduct =  productsData.filter(function(unisex) {
+    return unisex.sex == `unisex`;
+});
+
+// console.log(femaleproduct)
+
+function update_unisexproduct() {
+
+  mainProd.innerHTML = '';
+
+    for (let prounisex of unisexproduct) {
+      mainProd.insertAdjacentHTML('beforeend', `<tr><div class="main-item"> <div class="main-pic">
+      <img class="img_pro" wirdth="170" height="170" src="${prounisex.imageUrl}" alt="${prounisex.name}"/>
+  </div>
+  <div class="title">
+      <b>${ prounisex.brand}</b><br/>
+      <span>${ prounisex.price}</span>
+      
+  </div></td><button class="add_btns">Buy</button></td></div></tr>`);
+      };
+}
+
+
+
+
+let name = document.getElementById('name')
+let email = document.getElementById('email')
+let comments = document.getElementById('comments')
+
+function ShowModal(){
+  var MyModal = document.getElementById('myModal')
+  name.value="";
+  email.value="";
+  comments.value="";
+ MyModal.style.display=`block`
+}
+
+// When the user clicks on <span> (x), close the modal
+ function CloseModal() {
+  var MyModal = document.getElementById('myModal')
+  MyModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function() {
+//   var MyModal = document.getElementById('myModal')
+//   MyModal.style.display = "none";
+//   }
---
+++ perfume/Media/JS/data.js
@@ -0,0 +1,183 @@
+let admin ={
+    email:`amin123@gmail.com`,
+    password:`amin123`,
+    permission:`admin`,
+}
+
+let user = [
+    {
+        name: `Nguyen Huu Hop`,
+        age: 20,
+        address: `Ha Noi`,
+        phoneNumber: 033123456,
+        email: `hop123@gmail.com`,
+        password: `hop123`,
+        permission:`user`,
+
+    },{
+        name: `Phung Duc Trung`,
+        age: 20,
+        address: `Ha Noi`,
+        phoneNumber: 135791113,
+        email: `trung123@gmail.com`,
+        password: `trung123`,
+        permission:`user`,
+    },{
+        name: `Do Minh Khue`,
+        age: 20,
+        address: `Ha Noi`,
+        phoneNumber: 246810121,
+        email: `khue123@gmail.com`,
+        password: `khue123`,
+        permission:`user`,
+    }
+]
+
+let productsData = [
+    {
+        name:`La Nuit De L’homme EDT`,
+        brand:`Yves Saint Laurent`,
+        price:2950000,
+        imageUrl: `./Media/image/La Nuit De L’homme EDT.png` ,
+        size:`100ml`,
+        sex:`male`,
+        scent:[
+            'lavender',
+            'snowwood',
+            'cardamom',
+        ],
+    },{
+        name:`Libre`,
+        brand:`Yves Saint Laurent`,
+        price:3400000,
+        imageUrl:`./Media/image/Libre.png`,
+        size:`90ml`,
+        sex:`female`,
+        scent:[
+            'orange madarin',
+            'lavender',
+            'musk',
+        ],
+    }, {
+        name:`Dior Sauvage EDT`,
+        brand:`Dior`,
+        price:3150000,
+        imageUrl:`./Media/image/Dior Sauvage EDT.png`,
+        size:`100ml`,
+        sex:`male`,
+        scent:[
+            'black pepper',
+            'snowwood',
+            'ambroxan',
+        ],
+    }, {
+        name:`Sauvage Parfum`,
+        brand:`Dior`,
+        price:3850000,
+        imageUrl:`./Media/image/Sauvage Parfum.png`,
+        size:`100ml`,
+        sex:`male`,
+        scent:[
+            'vanilla',
+            'frankincense',
+            'sandalwood',
+
+        ],
+    }, {
+        name:`Chanel Coco Noir`,
+        brand:`Chanel`,
+        price:4250000,
+        imageUrl:`./Media/image/Chanel Coco Noir.png`,
+        size:`100ml`,
+        sex:`female`,
+        scent:[
+            'orange',
+            'rose',
+            'tonka beans',
+        ],
+    }, {
+        name:`Chanel Chance EDT`,
+        brand:`Chanel`,
+        price:3400000,
+        imageUrl:`./Media/image/Chanel Chance EDT.png`,
+        size:`100ml`,
+        sex:`female`,
+        scent:[
+            'pineapple',
+            'jasmine',
+            'vanilla',
+        ],
+    }, {
+        name:`Ombre Leather EDP`,
+        brand:`Tom Ford`,
+        price:4700000,
+        imageUrl:`./Media/image/Ombre Leather EDP.png`,
+        size:`100ml`,
+        sex:`male`,
+        scent:[
+            'leather',
+            'cardamom',
+            'moss moss',
+        ],
+    }, {
+        name:`Noir de Noir`,
+        brand:`Tom Ford`,
+        price:8000000,
+        imageUrl:`./Media/image/Noir de Noir.png`,
+        size:`100ml`,
+        sex: `unisex`,
+        scent:[
+            'saffron flowers',
+            'rose',
+            'oak tree',
+        ],
+    },{
+        name:`My Burberry Blush`,
+        brand:`Burberry`,
+        price:3750000,
+        imageUrl:`./Media/image/My Burberry Blush.png`,
+        size:`100ml`,
+        sex: `female`,
+        scent:[
+            'yellow lemon',
+            'rose',
+            'purple flowers',
+        ],
+    },{
+        name:`Mr. Burberry EDP`,
+        brand:`Burberry`,
+        price:3000000,
+        imageUrl:`./Media/image/Mr. Burberry EDP.png`,
+        size:`100ml`,
+        sex: `male`,
+        scent:[
+            'cinnamon',
+            'snowwood',
+            'amber',
+        ],
+    },{
+        name:`Twilly d’Hermès`,
+        brand:`Hermès`,
+        price:2950000,
+        imageUrl:`./Media/image/Twilly d’Hermès.png`,
+        size:`85ml`,
+        sex: `female`,
+        scent:[
+            'ginger',
+            'jasmine',
+            'sandalwood',
+        ],
+    },{
+        name:`Un Jardin Sur Le Nil`,
+        brand:`Hermès`,
+        price:2800000,
+        imageUrl:`./Media/image/Un Jardin Sur Le Nil.png`,
+        size:`100ml`,
+        sex: `unisex`,
+        scent:[
+            'pomelo',
+            'peony',
+            'musk',
+        ],
+    },
+]
